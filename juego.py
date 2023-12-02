db5a46e195582e8e6d10acc161aa3e2d2acd797d
LuisDA39 <xluis.da28@gmail.com>
2023-12-02 09:05:31 -0600

    jefe final agregado, musica modificada, correcciones y eso

--- juego.py
+++ juego.py
@@ -2,15 +2,16 @@ import os
 import random
 import time
 import pygame
+import string
 
 
 class Videogame:
     def __init__(self):
-        self.health = 2  # limitar la vida, no puede llegar a mas de 100 pts
+        self.health = 100  # limitar la vida, no puede llegar a mas de 100 pts
         self.inventory = []  # crear el inventario
         self.max_cap = 1
         self.current_space = 0
-        self.player_position = [0,3]
+        self.player_position = [0, 3]
         self.map = [
             [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],
             [0, 1, 0, 0, 1, 1, 0, 1, 0, 0],
@@ -399,7 +400,7 @@ class Videogame:
         self.move_player()
 
     def easter_egg1(self):
-        file_path = r'C:\Users\lloyd\Downloads\WhatsApp Audio 2023-11-25 at 15.51.39.mpeg'
+        file_path = r'soundfx/URSS.mpeg'
         self.play_music(file_path)
         print("""
             -%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -474,7 +475,7 @@ class Videogame:
         """)
 
     def easter_egg2(self):
-        file_path = r'C:\Users\lloyd\Downloads\Y2meta.app - Stranger Things _ Season 1 _ Opening - Intro HD (128 kbps).mp3'
+        file_path = r'soundfx/stranger things.mp3'
         self.play_music(file_path)
         print("""
                                                                              :=++**:                                      
@@ -535,7 +536,7 @@ class Videogame:
         """)
 
     def End_Game(self):
-        file_path = r'C:\Users\lloyd\Downloads\Y2meta.app - Pokemon Pueblo Lavanda Version Japonesa (Original) (128 kbps).mp3'
+        file_path = r'soundfx/lavender.mp3'
         self.play_music(file_path)
 
         self.print_slow("      Tony calls his mom, 'Hi… Hi… mom?... are you there?' Tony’s mom answers with a broken voice, 'Honey, so ")
@@ -567,10 +568,196 @@ class Videogame:
         """)
         time.sleep(3)
 
-    # crear historia y minijuego final
+        self.final_battle()
+
+    def final_battle(self):
+        boss_health = 700
+        player_health = self.health
+        damage_count = 0
+        phase = 1
+
+        print("Type swiftly and input the correct combination to unleash an attack on the boss.")
+        time.sleep(1.5)
+        while boss_health > 0 and player_health > 0:
+            k = phase + 2
+            damage = 0
+            combination = ''.join(random.choices(string.ascii_lowercase, k=k))
+            print(f"\nYour combination is: {combination}")
+
+            time.sleep(0.5)
+
+            start_time = time.time()
+            user_input = input("Enter the combination: ")
+            end_time = time.time()
+
+            if user_input != combination:
+                damage = random.randint(7, 10)
+                player_health -= damage
+                print(f"\nIncorrect combination. You lost {damage} health points")
+
+            elif end_time - start_time > 4:
+                damage = random.randint(7, 10)
+                player_health -= damage
+                print(f"\nToo slow. You lost {damage} health points")
+
+            else:
+                damage = random.randint(15, 30)
+                damage_count += damage
+                print(f"\nWell done! {damage} damage points to the boss")
+                boss_health -= damage
+
+            if boss_health < 0:
+                boss_health = 0
+
+            print("\n>Boss Health:", boss_health)
+            print(">Player Health:", player_health)
+
+            if damage_count > 70 and boss_health > 0:
+                event_num = random.randint(1, 4)
+
+                if event_num == 1:
+                    print("Boss is about to unleash a special attack. Get ready, type the combination backward!")
+                    time.sleep(1.5)
+                    damage = 0
+                    combination = ''.join(random.choices(string.ascii_lowercase, k=k))
+                    print(f"\nYour combination is: {combination}")
+                    time.sleep(0.5)
+                    start_time = time.time()
+                    user_input = input(" (!) Enter the combination in reverse order: ")
+                    end_time = time.time()
+
+                    if user_input != combination[::-1]:
+                        print("\nIncorrect combination")
+                        damage = random.randint(10, 20)
+                        print(f"Special attack! {damage} points less")
+
+                    elif end_time - start_time > 5:
+                        print("\nToo slow")
+                        damage = random.randint(10, 20)
+                        print(f"Special attack! {damage} points less")
+
+                    else:
+                        print("\nAttack dodged")
+
+                    player_health -= damage
+
+                if event_num == 2:
+                    print(
+                        "The boss has released his minions. Defeat them by entering the sum of the digits in the code.")
+                    time.sleep(2)
+
+                    damage = 0
+                    for i in range(3):
+                        start_time = time.time()
+                        num = random.randint(100, 999)
+                        print(f"\nYour number is: {num}")
+                        time.sleep(0.5)
+                        user_input = input(" (!) Enter the sum of the digits of the number: ")
+                        end_time = time.time()
+
+                        if int(user_input) != sum(int(digit) for digit in str(num)):
+                            print("\nIncorrect sum")
+                            damage += random.randint(6, 10)
+                            print(f"An ally is launching an attack against you! {damage} damage points to you")
+
+                        elif end_time - start_time > 6:
+                            print("\nToo slow")
+                            damage += random.randint(6, 15)
+                            print(f"Special attack! {damage} points less")
+
+                        else:
+                            print(f"\nYou defeted an ally {3 - i} left")
+
+                    player_health -= damage
+
+                if event_num == 3:
+                    print("You've found a medkit. Quickly press the designated key to heal yourself.")
+                    time.sleep(1.5)
+                    damage = 0
+                    random_letter = random.choice(string.ascii_lowercase)
+
+                    print(f"\n (!) Press '{random_letter}' 10 times in less than 5 seconds to heal.")
+                    time.sleep(0.5)
+                    start_time = time.time()
+                    user_input = input()
+                    end_time = time.time()
+
+                    if user_input.count(random_letter) < 10:
+                        print("\nYou haven't healed properly")
+                        damage = random.randint(10, 20)
+                        print(f"Damage! You lost {damage} health points")
+                        player_health -= damage
+
+                    elif end_time - start_time > 5:
+                        print("\nYou took too long.")
+                        damage = random.randint(10, 20)
+                        print(f"Damage! You lost {damage} health points")
+                        player_health -= damage
+
+                    else:
+                        heal = random.randint(5, 10)
+                        print(f"\nYou've healed! You gained {heal} health points")
+                        player_health += heal
+                        if player_health > 100:
+                            player_health = 100
+
+                if event_num == 4:
+                    print(
+                        "The boss is gearing up for a special attack. Guess the correct number to minimize the damage.")
+                    time.sleep(1.5)
+                    start_time = time.time()
+                    secret_number = random.randint(1, 20)
+                    attempts = 0
+
+                    print("\nGuess a number between 1 and 20.")
+                    time.sleep(0.5)
+
+                    while True:
+                        user_input = input("\n(!) Please enter a number: ")
+                        end_time = time.time()
+
+                        if not user_input.isdigit():
+                            print("Enter a valid number.")
+                            continue
+
+                        number = int(user_input)
+                        attempts += 1
+
+                        if number < secret_number:
+                            print("Too low! Try again.")
+                        elif number > secret_number:
+                            print("Too high! Try again.")
+                        else:
+                            if end_time - start_time > 10:
+                                print(f"\nCorrect, but you took too long! The boss dealt a critical hit to you.")
+                                damage = random.randint(10, 15)
+                                print(f"Damage! You lost {damage} health points")
+                                player_health -= damage
+                            else:
+                                print(
+                                    f"\nCorrect! You guessed the number and managed to shield yourself from the attack.")
+                                player_health -= attempts
+                            break
+
+                damage_count = 0
+
+            if player_health <= 0:
+                print("\nYou lost.")
+                player_health = 0
+                break
+
+            if boss_health <= 0:
+                print("\nYou won!")
+                break
+
+            if 500 > boss_health > 250:
+                phase = 2
+
+            if boss_health < 250:
+                phase = 3
 
     def Game_Over(self):
-        file_path = r'C:\Users\lloyd\Downloads\Y2meta.app - Billie Eilish - lovely (with Khalid) Audio (128 kbps).mp3'
+        file_path = r'soundfx/lovely.mp3'
         self.play_music(file_path)
         print("""
 
@@ -600,7 +787,7 @@ class Videogame:
         time.sleep(28)
 
     def Title(self):
-        file_path = r'C:\Users\lloyd\OneDrive\Documents\UP\3er Semestre\Algoritmos\Videogame\everything i wanted.mp3'
+        file_path = r'soundfx/everything i wanted.mp3'
         self.play_music(file_path)
 
         print("""
@@ -830,8 +1017,8 @@ class Videogame:
                                 option = input('>').lower().strip()
                                 if option in ['yes', 'eat', 'enjoy', 'no', 'later', 'now']:
                                     if option in ['yes', 'eat', 'enjoy', 'now']:
-                                        self.print_slow("      Delicious! You ate an apple, and your health increased by 5 points.")
-                                        self.health += 5
+                                        self.print_slow("      Delicious! You ate an apple, and your health increased by 7 points.")
+                                        self.health += 7
                                         self.visited.append(self.player_position)
                                         if self.health > 100:
                                             self.health = 100
@@ -846,7 +1033,7 @@ class Videogame:
                                 break
 
                     # screwdriver
-                    if self.player_position == [2,1] and self.player_position not in self.visited:
+                    if self.player_position == [2, 1] and self.player_position not in self.visited:
                         print("""
                                                                                                  
                                                                  
@@ -1033,7 +1220,7 @@ class Videogame:
                                 print("Not the word we're looking for.\n")
 
                     if (self.player_position == [9, 8] and [9, 9] in self.visited) or (self.player_position == [1, 9] and [3, 0] in self.visited):
-                        file_path = r'everything i wanted.mp3'
+                        file_path = r'soundfx/everything i wanted.mp3'
                         self.play_music(file_path)
 
                     # broke down car
@@ -1342,7 +1529,7 @@ class Videogame:
                                 self.print_slow("You cant do that right now.")
 
                     # coins
-                    if self.player_position == [5,9] and self.player_position not in self.visited:
+                    if self.player_position == [5, 9] and self.player_position not in self.visited:
                         print("""
                                                        :*#%%%%%@@@@@@@%+                   
                                                   *#%%%#++===========++#%%%@@-             
@@ -1407,7 +1594,7 @@ class Videogame:
 
                     # Minigame
                     if self.player_position == [8, 5] and self.player_position not in self.visited:
-                        file_path = r'C:\Users\lloyd\Downloads\Y2meta.app - Lobo aullando-efecto de sonido (128 kbps).mp3'
+                        file_path = r'soundfx/lobo.mp3'
                         self.play_music(file_path)
                         print("""                                                                    
                                                                               ..                
@@ -1445,9 +1632,9 @@ class Videogame:
                                                                     
                         """)
                         time.sleep(5)
-                        file_path = r'C:\Users\lloyd\Downloads\Y2meta.app - Star Wars - Battle of the Heroes Suite (128 kbps).mp3'
+                        file_path = r'soundfx/starwars.mp3'
                         self.play_music(file_path)
-                        self.visited.append(self.player_position) # Maybe aqui un ascii del lobo
+                        self.visited.append(self.player_position)
                         self.print_slow("      Navigating through the forest, the tranquility shatters as a distant growl reveals a lurking wolf.")
                         self.print_slow("      Adrenaline surges, and you sprint through the trees. Amidst the chaos, you stumble upon a suitcase.")
                         self.print_slow("      Flipping it open, relief washes over as you spot a firearm. However, it's locked with a combination.")
@@ -1493,7 +1680,7 @@ class Videogame:
                             self.print_slow("      Despite your efforts, the wolf catches up.")
                             self.print_slow("      You manage to escape, but the wolf leaves you severely injured.")
                             self.print_slow("      Unfortunately, you've lost 50 health points in the encounter.")
-                            self.health -= 10
+                            self.health -= 50
                         else:
                             self.print_slow("      You quickly grab the weapon, fend off the wolf, and escape unscathed. Well done!")
 
@@ -1532,7 +1719,6 @@ class Videogame:
                                      --:::::::::::::::::::::::::::::::::::::::::::::::::::::::- 
 
                         """)
-                        #corregir
                         self.print_slow("A telephone booth is right in front of you, isn't that weird?... ANYWAYS, do you want to use it?")
                         op = input("Type 'yes' or 'no' ").lower().strip()
 
@@ -1566,20 +1752,25 @@ class Videogame:
                                                                                     self.print_slow("A digit is missing, guess it")
                                                                                     secret_number = 8
                                                                                     attempts = 0
+                                                                                    max_attempts = 5
 
                                                                                     while True:
                                                                                         user_guess = input("Enter your guess: ")
 
                                                                                         attempts += 1
 
-                                                                                        if int(user_guess) == secret_number: #si adivinas el numero que no te lo siga pidiendo
+                                                                                        if int(user_guess) == secret_number:
                                                                                             print(f"Congratulations! You guessed the number in {attempts} attempts.")
                                                                                             self.print_slow(       "Processing . . . ")
                                                                                             time.sleep(3)
                                                                                             self.End_Game()
-                                                                                        else: #si no adinas el numero, llmar a la funcion game over
+                                                                                            break
+                                                                                        elif max_attempts > attempts:
                                                                                             print("Incorrect. Try again.")
-                                                                                            user_guess = input("Enter your guess: ")
+
+                                                                                        if max_attempts == attempts:
+                                                                                            self.Game_Over()
+                                                                                            break
 
                                                                                 else:
                                                                                     self.print_slow("Go find a phone number")
@@ -1608,7 +1799,7 @@ class Videogame:
                                                     break
                                                 break
                                             else:
-                                               self.print_slow("That's not quite an option\n")
+                                                self.print_slow("That's not quite an option\n")
                                     
                                         break
                                     else:
@@ -1662,6 +1853,7 @@ class Videogame:
             else:
                 self.Game_Over()
                 exit(0)
+
 
 vg = Videogame()
 vg.Title()
